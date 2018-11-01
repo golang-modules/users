709186ca1d24ccb55a87f722f427e7ae0116b567
Chaganti <rchaganti@TX717-0ASHTDH.local>
2018-11-01 14:24:59 -0500

    Added json tags to struct

--- models/user.go
+++ models/user.go
@@ -2,10 +2,10 @@ package models
 
 //User model for saving user
 type User struct {
-	ID        int64
-	FirstName string
-	LastName  string
-	Email     string
-	Password  string
-	Active    bool
+	ID        int64  `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Active    bool   `json:"active"`
 }
